9b23622e9eff4cdc4039e506ce484f18e6e7bb9f
Naila Shafa <naila.shafa41@ui.ac.id>
2026-03-31 15:24:45 +0700

    Revert "Author"

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.landing_page, name='landing_page'),
-    path('tampilan/edit/', views.update_tampilan, name='update_tampilan'),
-    path('profil/edit/<int:id>/', views.update_biodata, name='update_profil'),
-    path('logout/', views.logout_view, name='logout'),
-]
